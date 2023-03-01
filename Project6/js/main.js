43f70b2adc16b1e77e3576d58c1639a117baba96
iosos <osama.abdulhaleem@gmail.com>
2023-03-01 20:32:54 +0100

    Add files via upload

---
+++ Project6/js/main.js
@@ -0,0 +1,22 @@
+// (function ($) {
+
+
+	$(document).ready(function(){
+		"use strict";
+
+	/*--/ Testimonials owl /--*/
+	$('#testimonial-mf').owlCarousel({
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			}
+		}
+	});
+	});
+	
+
+// })(jQuery);
